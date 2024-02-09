9322f2f2f821950e45fc8abe15e05d3ceddfe3e8
paul449 <paul_bilbatua@hotmail.com>
2024-02-09 14:23:15 -0600

    deleted file and modifying script file

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function GenerateButton(){
+
+  var prompt1 = prompt("How many characters would you like for your password to contain?:");
+  var confirm1 = confirm("Click ok to confirm including numeric characters");
+  var confirm2 = confirm("Click OK to confirm lowercase characters");
+  var confirm3 = confirm("Click OK to confirm uppercase characters");
+}
+
+
 // Write password to the #password input
+
+var passCharacters = "";
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
